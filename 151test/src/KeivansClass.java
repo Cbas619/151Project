c34e3d4b32802193f025cee5f3a85382043c436b
keivanhatami <keivanh00>
2020-11-08 20:30:40 -0800

    keivans commit

--- 151test/src/KeivansClass.java
+++ 151test/src/KeivansClass.java
@@ -1,14 +1,14 @@
 
-public class TesterClass {
+public class KeivansClass {
+	
 	public static void main(String[] args) {
 		System.out.println("Sebastian");
 		System.out.println("Keivan's commit");
 		System.out.println("Brian");
-		
-		
-		
+	
 		
 		
 		
 	}
+
 }
