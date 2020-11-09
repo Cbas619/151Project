b976af0491d2ebd04db92594487ea0b89991275e
brhuynh <hbrian2008@gmail.com>
2020-11-08 20:24:32 -0800

    brian's commit

--- 151test/src/TesterClass.java
+++ 151test/src/TesterClass.java
@@ -3,5 +3,6 @@ public class TesterClass {
 	public static void main(String[] args) {
 		System.out.println("Sebastian");
 		System.out.println("Keivan's commit");
+		System.out.println("Brian");
 	}
 }
