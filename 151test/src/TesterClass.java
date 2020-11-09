16ece261bc5d7a635431569ffef274210a8a158c
keivanhatami <keivanh00>
2020-11-08 20:22:21 -0800

    second commit

--- 151test/src/TesterClass.java
+++ 151test/src/TesterClass.java
@@ -2,6 +2,6 @@
 public class TesterClass {
 	public static void main(String[] args) {
 		System.out.println("Sebastian");
-		System.out.println("Keivan");
+		System.out.println("Keivan's commit");
 	}
 }
