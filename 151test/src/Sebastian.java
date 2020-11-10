daba9e3b9d2ecd3158e12f320558dd7afc565feb
Cbas <Cbas@10.0.0.167>
2020-11-09 21:10:08 -0800

    added print statements

--- 151test/src/Sebastian.java
+++ 151test/src/Sebastian.java
@@ -6,6 +6,10 @@ public class Sebastian {
 		int y = 10;
 		int sum = x+y;
 		System.out.println("What's nine + ten? " + sum);
+		System.out.println("My");
+		System.out.println("Name");
+		System.out.println("is");
+		System.out.println("Sebastian");
 	}
 
 }
